df519110dd88e1cd3deb5b84f87fce0e0e5546d6
Klovern <oskar.kindeland@gmail.com>
2023-09-30 23:36:58 +0200

    Init commit UserServiceControllerAPI

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Health")]
+        public ActionResult Health()
+        {
+            return Ok("--> Healthy");
+        }
+    }
+}
