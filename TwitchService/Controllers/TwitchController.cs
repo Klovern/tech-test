f6383b5665095ae1e7ca3b53b1e29361965f6d9c
Klovern <oskar.kindeland@gmail.com>
2023-10-01 14:21:15 +0200

    Added twitch-controller

---
+++ TwitchService/Controllers/TwitchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwitchService.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwitchController : ControllerBase
+    {
+        public TwitchController()
+        {
+                
+        }
+
+        public ActionResult Index()
+        {
+            return Ok("---> Alive");
+        }
+    }
+}
